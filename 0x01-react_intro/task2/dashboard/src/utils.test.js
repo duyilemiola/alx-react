663bcbc71edca0dd790e5776d93a2d0460c6f21f
olaoluwa omoduyilemi <88881222+duyilemiola@users.noreply.github.com>
2023-08-24 04:16:10 +0100

    Create utils.test.js

---
+++ 0x01-react_intro/task2/dashboard/src/utils.test.js
@@ -0,0 +1,6 @@
+
+// Write a test to check that the function getFullYear returns the correct year(be careful to not create a time bomb)
+// Write a test to check that getFooterCopy returns the correct string when the argument is true or false
+// Write a test checking the returned string for getLatestNotification
+
+
